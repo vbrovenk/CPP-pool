190f2125acce46fcd1837d454ce3209d3bc06f6d
Vadim BROVENKO <vbrovenk@e1r6p6.unit.ua>
2019-07-05 17:33:45 +0300

    Day07 is done

--- Day07/ex02/Array.hpp
+++ Day07/ex02/Array.hpp
@@ -4,8 +4,60 @@
 template <typename T>
 class Array
 {
+private:
+	unsigned int _n;
+	T *_a;
+
+
 public:
-	Array(void) {};
+	Array<T>(void) : _n(0)
+	{
+
+	}
+
+	Array<T>(unsigned int n) : _n(n)
+	{
+		_a = new T[n];
+
+		// for (unsigned int i = 0; i < n; i++)
+		// 	_a[i] = 0;
+	}
+
+	Array<T>(Array<T> const &other) 
+	{
+		*this = other;
+	}
+
+	~Array<T>(void)
+	{
+		if (_n > 0)
+			delete [] _a;
+	}
+
+	unsigned int size() const { return _n; }
+
+	Array<T> & operator =(Array<T> const &other) 
+	{
+		if (this != &other)
+		{
+			if (_n > 0)
+				delete [] _a;
+			_n = other._n;
+
+			_a = new T[_n];
+			for (unsigned int i = 0; i < _n; i++)
+				_a[i] = other._a[i];
+		}
+
+		return *this;
+	}
+
+	T &operator [](unsigned int index)
+	{
+		if (index >= _n)
+			throw std::exception();
+		return _a[index];
+	}
 };
 
 
--- Day07/ex02/main.cpp
+++ Day07/ex02/main.cpp
@@ -1,9 +1,106 @@
 #include <iostream>
 #include "Array.hpp"
 
+void check()
+{
+	std::cout << "============= DOUBLE =============" << std::endl;
+	Array<float> ar(10);
+	Array<float> arr2(15);
+
+	std::cout << "============= BEFORE =============" << std::endl;
+
+	for (unsigned int i = 0; i < ar.size(); i++)
+	{
+		ar[i] = i;
+		std::cout << ar[i] << std::endl;
+	}
+	std::cout << "=========================" << std::endl;
+
+	for (unsigned int i = 0; i < arr2.size(); i++)
+	{
+		arr2[i] = i;
+		std::cout << arr2[i] << std::endl;
+	}
+
+	ar = arr2;
+	std::cout << "============= AFTER =============" << std::endl;
+
+	for (unsigned int i = 0; i < ar.size(); i++)
+	{
+		std::cout << ar[i] << std::endl;
+	}
+	std::cout << "=========================" << std::endl;
+
+	for (unsigned int i = 0; i < arr2.size(); i++)
+	{
+		std::cout << arr2[i] << std::endl;
+	}
+
+}
+
+void check2()
+{
+	std::cout << "============= STRING =============" << std::endl;
+	Array<std::string> ar(10);
+	Array<std::string > arr2(15);
+
+	std::cout << "============= BEFORE =============" << std::endl;
+
+	for (unsigned int i = 0; i < ar.size(); i++)
+	{
+		ar[i] = "hello";
+		std::cout << ar[i] << std::endl;
+	}
+	std::cout << "=========================" << std::endl;
+
+	for (unsigned int i = 0; i < arr2.size(); i++)
+	{
+		arr2[i] = "world";
+		std::cout << arr2[i] << std::endl;
+	}
+
+	ar = arr2;
+	std::cout << "============= AFTER =============" << std::endl;
+
+	for (unsigned int i = 0; i < ar.size(); i++)
+	{
+		std::cout << ar[i] << std::endl;
+	}
+	std::cout << "=========================" << std::endl;
+
+	for (unsigned int i = 0; i < arr2.size(); i++)
+	{
+		std::cout << arr2[i] << std::endl;
+	}
+
+}
+
+
 int main(void)
 {
+	Array<int> ar(5);
+	Array<int> arr2(6);
 
+	std::cout << "==================================" << std::endl;
+	for (unsigned int i = 0; i < ar.size(); i++)
+		std::cout << ar[i] << std::endl;
+
+	arr2 = ar;
+	std::cout << "==================================" << std::endl;
+	for (unsigned int i = 0; i < ar.size(); i++)
+		std::cout << ar[i] << std::endl;
+
+	std::cout << "============= TRY CATCH =============" << std::endl;
+	try
+	{
+		std::cout << arr2[-1] << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Out of range" << std::endl;
+	}
+	check();
+	check2();
 
 	return 0;
 }
